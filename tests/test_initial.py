29773afa34270a88390610c48164d3b065e9e6f2
Mason Bassett <bassettmason@gmail.com>
2023-07-17 14:58:50 -0700

    spacing in initial test

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -3,4 +3,5 @@ from pylenium.driver import Pylenium
 
 def test_bassettmason(py: Pylenium):
     py.visit('https://www.bassettmason.com/')
+    
     assert py.get('h2.elementor-heading-title.elementor-size-default').should().contain_text('Mason Bassett')
